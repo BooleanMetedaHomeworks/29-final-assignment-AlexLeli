7f7f0f413ff69a4c77a4c7dc0e4a4d3b372c9479
AlexLeli <alex.leli@meteda.it>
2025-03-05 18:11:28 +0100

    main window

---
+++ ristorante-frontend/Models/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ristorante_frontend.Models;
+
+namespace ristorante_frontend.NewFolder
+{
+    public class Dish
+    {
+        public int ID_Dish { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
---
+++ ristorante-frontend/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ristorante_frontend.Models
+{
+    public class Menu
+    {
+        public int ID_Menu { get; set; }
+        public string Name { get; set; }
+    }
+}
